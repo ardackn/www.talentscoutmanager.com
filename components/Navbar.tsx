93470b11ba8deb920d1cbf84b073126b93538484
Arda <ardackn@gmail.com>
2026-04-25 06:15:40 +0300

    fix: resolve global ReferenceError by adding missing React hooks to Navbar imports and cleaning up directives

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useSession } from '@/hooks/use-session'
 import { useTranslation } from 'react-i18next'
